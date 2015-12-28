e0a2d7c87c40b87cd1a4a0202133eed9448b0e3b
Matus Kral <matuskral@me.com>
2015-12-28 01:21:25 +0100

    fix: debian/*.install, git revision

--- src/libcec/cmake/git-rev.sh
+++ src/libcec/cmake/git-rev.sh
@@ -2,7 +2,7 @@
 
 ## cmake doesn't read the variable when it doesn't end with a newline, and I haven't figured out how to have it add a newline directly...
 if git rev-parse --git-dir > /dev/null 2>&1; then
-  last_tag=`git describe --tags --abbrev=0`
+  last_tag=`git describe --tags --abbrev=0 --all`
   last_hash=`git --no-pager log --abbrev=7 -n 1 --pretty=format:"%h"`
   commits_since_tag=`git log ${last_tag}..HEAD --oneline | wc -l`
   git_dirty=`git diff HEAD | wc -l`
